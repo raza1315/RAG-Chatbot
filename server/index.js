505f80f6b9ce2ff973a38c114bd5e2bae49211dd
raza1315 <razasfs2003@gmail.com>
2026-02-06 17:21:24 +0530

    Producer done

--- server/index.js
+++ server/index.js
@@ -4,12 +4,24 @@ const cors = require("cors");
 const {Queue} = require("bullmq");
 const multer = require("multer");
 
+// Create Queue instance from Queue Class
+const queue = new Queue("pdf-upload-queue",{connection:{host:'localhost',port:6379}});
+
 const app = express();
 const PORT = 8000;
 app.use(cors());
 
 app.get("/",(req,res)=>{
  res.status(200).json({message:"This is the Home Route"});
+})
+
+app.post("/produce-sample-job",async(req,res)=>{
+ //add job to the queue
+ const data = {color:"green",model:1234};
+ const job_name = "upload-file";
+ const message = JSON.stringify(data);
+ await queue.add(job_name,message);
+ res.status(200).json({message:"uploaded successfully"});
 })
 
 app.listen(PORT,()=>{
