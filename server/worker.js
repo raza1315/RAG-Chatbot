6735d2e5f9898378fa0d10637fa38a0cbd77f34d
raza1315 <razasfs2003@gmail.com>
2026-02-06 17:30:53 +0530

    sample Worker done

---
+++ server/worker.js
@@ -0,0 +1,25 @@
+const {Worker} = require("bullmq");
+
+const worker = new Worker(
+ 'file-upload-queue',
+ async (job)=>{
+  const data = JSON.parse(job.data);
+  console.log("Job Data: ",data);
+ },
+ {
+ concurrency: 1,
+ connection: {host:'localhost',port:6379}
+ }
+)
+
+//Worker events: 
+
+worker.on('completed', (job)=>{
+ console.log(`Job ${job.id} completed successfully`);
+})
+
+worker.on('failed',(job,err)=>{
+ console.log(`Job ${job.id} failed --> Error: ${err.message}`)
+})
+
+console.log("Worker Started and listening for jobs...")
